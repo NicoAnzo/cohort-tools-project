23a5082480d819a1ad3b38817ecf12c85dc343b3
Nicolas Anzoategui <nanzoate@gmail.com>
2024-08-26 16:57:03 +0200

    initial installations and set up

--- server/app.js
+++ server/app.js
@@ -3,10 +3,14 @@ const morgan = require("morgan");
 const cookieParser = require("cookie-parser");
 const PORT = 5005;
 
+const cors = require("cors");
+
+
 // STATIC DATA
 // Devs Team - Import the provided files with JSON data of students and cohorts here:
 // ...
-
+const cohorts = require("./cohorts.json");
+const students = require("./students.json");
 
 // INITIALIZE EXPRESS APP - https://expressjs.com/en/4x/api.html#express
 const app = express();
@@ -15,6 +19,14 @@ const app = express();
 // MIDDLEWARE
 // Research Team - Set up CORS middleware here:
 // ...
+app.use(
+  cors({
+    // Add the URLs of allowed origins to this array
+    origin: ['http://localhost:5005'],
+  })
+);
+
+
 app.use(express.json());
 app.use(morgan("dev"));
 app.use(express.static("public"));
@@ -27,6 +39,14 @@ app.use(cookieParser());
 // ...
 app.get("/docs", (req, res) => {
   res.sendFile(__dirname + "/views/docs.html");
+});
+
+app.get("/api/cohorts", (req, res) => {
+  res.json(cohorts);
+});
+
+app.get("/api/students", (req, res) => {
+  res.json(students);
 });
 
 
